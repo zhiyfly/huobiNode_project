40b0c7b7a49f67ceaa9a8ad47a6f2589d1bd531d
matao <matao04@meituan.com>
2018-07-22 14:02:05 +0800

    demo

--- demo_currency.js
+++ demo_currency.js
@@ -1,12 +1,10 @@
 const hb_currency = require('./sdk/hb_currency');
+var hbconfig = require('./job/hbconfig');
+
+var config = hbconfig.hbconfig;
+
 
 let  run = ()=>{
-    var config = {
-        access_key : '0e44466a-072c6d96-121dc8f5-bd52d',
-        secretkey : '079f54ea-165848f7-aefbc94c-70cea'
-    }
-    hb_currency.get_currency('xrpusdt',config).then(console.log);
-    // console.log(json);
-    
+    hb_currency.get_currency('eosusdt',config).then(console.log);
 }
 run();
